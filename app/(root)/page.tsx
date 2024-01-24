3dcaf59887effaf1f185ffe740649a008a1cf5a5
Muhammad Rifqi Setiawan <muhammadrifqi142@gmail.com>
2024-01-24 17:12:33 +0700

    initialization project configuration and add home pages

---
+++ app/(root)/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1 className="head-text text-left text-white">Home</h1>
+    </div>
+  );
+}
---
+++ components/shared/Topbar.tsx
@@ -0,0 +1,34 @@
+"use client";
+import {
+  OrganizationSwitcher,
+  SignOutButton,
+  SignedIn,
+} from "@clerk/clerk-react";
+import Image from "next/image";
+import Link from "next/link";
+
+function Topbar() {
+  return (
+    <nav className="topbar">
+      <Link href="/" className="flex items-center gap-4">
+        <Image src="logo.svg" alt="logo" width={28} height={28} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
+      </Link>
+
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          
+        </div>
+        <OrganizationSwitcher
+          appearance={{
+            elements: {
+              organizationSwitcherTrigger: "py-2 px-4",
+            },
+          }}
+        />
+      </div>
+    </nav>
+  );
+}
+
+export default Topbar;
